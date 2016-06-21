1b2c7547b538e338338ab928d93fcdbb04af42d6
Sam Biondolillo <biwinning121@gmail.com>
2016-06-21 21:18:51 +0000

    Handle game's end logic

--- app.rb
+++ app.rb
@@ -53,7 +53,8 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
+    redirect '/win' if @game.check_win_or_lose == :win
+    redirect '/lose' if @game.check_win_or_lose == :lose
     erb :show # You may change/remove this line
   end
   
